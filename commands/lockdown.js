81425926dfa8065162ea28703b957365a2ebd524
GlandorfGamer <63344328+GlandorfGamer@users.noreply.github.com>
2021-07-04 21:12:28 +0200

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -3,6 +3,26 @@ module.exports={
     description: "Puts a channel on a lockdown",
     async execute(message, args, Discord){
 
+        
+        if(args[0]=="reset"){
+               if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You don't have the permissions for that!");
+        const channel = message.mentions.channels.first();
+        if(!channel) return message.channel.send("This channel does not exist!");
+        const rolefind = args.slice(1).join(" ");
+        const role = message.guild.roles.cache.find(r => r.id === rolefind);
+        if(!role) return message.channel.send("Please give a valid role id!");
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Channel Unlocked!")
+        .setDescription(`This channel has been unlocked by ${message.author.tag}`)
+        .setTimestamp();
+        channel.updateOverwrite(role, {
+            SEND_MESSAGES: true
+        });
+        await channel.send(embed)
+        }
+        
+        
+        
         if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You don't have the permissions for that!");
         const channel = message.mentions.channels.first();
         if(!channel) return message.channel.send("This channel does not exist!");
