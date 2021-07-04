40d8d5ea77991d6d465053ff601e0a31848a95dd
GlandorfGamer <63344328+GlandorfGamer@users.noreply.github.com>
2021-07-04 21:57:06 +0200

    Create reslockdown.js

---
+++ commands/reslockdown.js
@@ -0,0 +1,28 @@
+  
+module.exports={
+    name: "reslockdown",
+    description: "Resets the Lockdown",
+    async execute(message, args, Discord){
+        
+        
+        if(args[0]=="reset"){
+        if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You don't have the permissions for that!");
+        const channel = message.mentions.channels.first();
+        if(!channel) return message.channel.send("This channel does not exist!");
+        const rolefind = args.slice(2).join(" ");
+        const role = message.guild.roles.cache.find(r => r.id === rolefind);
+        if(!role) return message.channel.send("Please give a valid role id!");
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Channel Unlocked!")
+        .setDescription(`This channel has been Unlocked by ${message.author.tag}`)
+        .setTimestamp();
+        channel.updateOverwrite(role, {
+            SEND_MESSAGES: false
+        });
+        await channel.send(embed)
+        }
+        
+        
+        
+    }
+}
