faa720b378d573a957276e9e01cad41ddb44b5d4
GlandorfGamer <63344328+GlandorfGamer@users.noreply.github.com>
2021-07-04 20:09:22 +0200

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,1 +1,16 @@
+module.exports={
+    name: "lockdown",
+    description: "Puts a channel on a lockdown",
+    execute(message, args){
 
+        if(!message.member.permissions.has("BAN_MEMBERS")){
+            message.reply('You are not allowed to do that!');
+            return;
+        }
+
+        const role = message.guild.roles.everyone;
+      
+      if(!args.length) return message.channel.send("Please specify a query");
+
+    }
+}
