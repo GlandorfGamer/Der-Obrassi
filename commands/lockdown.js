a7c8f245e995dfa50f2a364c231a91d81035a238
GlandorfGamer <63344328+GlandorfGamer@users.noreply.github.com>
2021-07-04 21:01:16 +0200

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,19 +1,21 @@
 module.exports={
     name: "lockdown",
     description: "Puts a channel on a lockdown",
-    execute(message, args){
+    async execute(message, args){
 
-        if(!message.member.permissions.has("BAN_MEMBERS")){
-            message.reply('You are not allowed to do that!');
-            return;
-        }
-
-        const role = message.guild.roles.everyone;
-      
-      if(!args.length) return message.channel.send("Please specify a query");
-        
-        const query = args[0].toLowerCase();
-        
-        if(!['true', 'false'].includes(query)) return message.channel.send("The option you have stated is not valid!");
+        if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You don't have the permissions for that!");
+        const channel = message.mentions.channels.first();
+        if(!channel) return message.channel.send("This channel does not exist!");
+        const rolefind = args.slice(1).join(" ");
+        const role = message.guild.roles.cache.find(r => r.id === rolefind);
+        if(!role) return message.channel.send("Please give a valid role id!");
+        let embed = new MessageEmbed()
+        .setImage("Channel Locked!")
+        .setDescription(`This channel has been locked by ${message.author.tag}`)
+        .setTimestamp();
+        channel.updateOverwrite(role, {
+            SEND_MESSAGES: false
+        });
+        await channel.send(embed)
     }
 }
