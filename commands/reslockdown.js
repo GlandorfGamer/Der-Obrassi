b66e019de08f8d402f1d9a382f3d313f61df1bde
GlandorfGamer <63344328+GlandorfGamer@users.noreply.github.com>
2021-07-04 22:02:12 +0200

    Update reslockdown.js

--- commands/reslockdown.js
+++ commands/reslockdown.js
@@ -5,7 +5,7 @@ module.exports={
     async execute(message, args, Discord){
         
         
-        if(args[0]=="reset"){
+   
         if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("You don't have the permissions for that!");
         const channel = message.mentions.channels.first();
         if(!channel) return message.channel.send("This channel does not exist!");
@@ -20,7 +20,7 @@ module.exports={
             SEND_MESSAGES: false
         });
         await channel.send(embed)
-        }
+        
         
         
         
