3a62a289e54a24e6ef5f44b49177ffe5aa3f0ee0
GlandorfGamer <63344328+GlandorfGamer@users.noreply.github.com>
2021-07-04 21:55:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ client.on('message', message =>{
         client.commands.get('imagine').execute(message, args, Discord);
     }else if(command === 'reslockdown'){
         client.commands.get('reslockdown').execute(message, args, Discord);
+    }else if(command === 'lockdown'){
+        client.commands.get('lockdown').execute(message, args, Discord);
     }
 })
 
